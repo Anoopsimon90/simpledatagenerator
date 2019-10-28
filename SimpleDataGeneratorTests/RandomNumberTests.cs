24084efd7e4527fbe8f7d1bae9f7de8d4948de0d
Anoop Simon <anoop.simon@adudev.com>
2019-10-28 16:39:08 +1100

    Added test category

--- SimpleDataGeneratorTests/RandomNumberTests.cs
+++ SimpleDataGeneratorTests/RandomNumberTests.cs
@@ -8,6 +8,7 @@ namespace SimpleDataGeneratorTests
     public class RandomNumberTests
     {
         [Theory]
+        [Trait("Module", "RandomNumber")]
         [InlineData(10,10)]
         [InlineData(0, 0)]
         [InlineData(9999, 9999)]
@@ -20,13 +21,15 @@ namespace SimpleDataGeneratorTests
         }
 
         [Fact]
+        [Trait("Module", "RandomNumber")]
         public void VerifyMaxNumberOfRecordsIsSetTo10000()
         {
             ISimpleDataGeneratorService simpleDataGeneratorService = new SimpleDataGeneratorService();
+            const string EXCEPTION_MESSAGE = "Maximum limit is 10000";
 
-           var exception= Assert.Throws<OverflowException>(() => simpleDataGeneratorService.GenerateRandomNumbers(10001));
+            var exception = Assert.Throws<OverflowException>(() => simpleDataGeneratorService.GenerateRandomNumbers(10001));
 
-            Assert.True(exception.Message == "Maximum limit is 10000");
+            Assert.True(exception.Message == EXCEPTION_MESSAGE);
         }
     }
 }
